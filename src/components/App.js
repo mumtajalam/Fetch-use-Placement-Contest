7c2364e4d7f2523f9484c8709984394090659502
md_mumtaj_alam <mmumtajalam987@gmail.com>
2022-10-09 17:17:34 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,29 @@
-import React from 'react'
-import '../styles/App.css';
-import { useState, useEffect } from 'react';
+import React from "react";
+import "../styles/App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 const App = () => {
-//code here 
- 
+  //code here
+  const [input, setInput] = useState("");
+  const [name, setName] = useState("");
 
+  const changeInput = (e) => {
+    setInput(e.target.value);
+  };
 
+  const callapi = async () => {
+    const userid = parseInt(input);
+    console.log(userid);
+    const response = await axios.get(
+      `https://content.newtonschool.co/v1/pr/main/users/${input}`
+    );
+    console.log(response.data);
+    setName(response.data.name);
+  };
+
+  useEffect(() => {
+    callapi();
+  }, [input]);
 
   return (
     <div className="App">
@@ -14,7 +32,6 @@ const App = () => {
       <p id="name">{name}</p>
     </div>
   );
-}
-
+};
 
 export default App;
